10f36744d3dd2afaa70e69fd5d645f10992ac791
은영 <e_yeong@naver.com>
2022-07-30 04:33:56 +0900

    Change the CNN architecture

--- TN/code/model.py
+++ TN/code/model.py
@@ -4,9 +4,9 @@ import torch.nn as nn
 class CNN_Classifier(nn.Module):
     def __init__(self):
         super(CNN_Classifier, self).__init__()
-        self.cnn1 = nn.Conv1d(in_channels=20, out_channels=11, kernel_size=5, padding=2, stride=1)
-        self.cnn2 = nn.Conv1d(in_channels=11, out_channels=5, kernel_size=5, padding=2, stride=1)
-        self.cnn3 = nn.Conv1d(in_channels=5, out_channels= 1, kernel_size=5, padding=2, stride=1)
+        self.cnn1 = nn.Conv1d(in_channels=20, out_channels=11, kernel_size=4, padding=2, stride=2)
+        self.cnn2 = nn.Conv1d(in_channels=11, out_channels=5, kernel_size=4, padding=2, stride=2)
+        self.cnn3 = nn.Conv1d(in_channels=5, out_channels= 1, kernel_size=4, padding=2, stride=2)
         self.relu = nn.ReLU()
 
     def forward(self, x):
